05bda5d8958d30f7b8ab8d5ff410c11da26baf49
WKSefton <william.k.sefton@gmail.com>
2022-02-21 11:34:48 -0500

    initial skeleton

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.imgur.com'],
+    domains: ['www.pexels.com'],
+  },
 }
 
 module.exports = nextConfig
